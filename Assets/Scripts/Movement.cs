e1faf52f9811d703710ac17d181f4960be1da3bd
Ekrem İbram <32652972+Echos-dll@users.noreply.github.com>
2022-04-22 14:53:40 +0300

    Movement
    +Scene setup
    +Added movement script
    +Added models and animations

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public enum Directions
+{
+    Straight,
+    Left,
+    Right
+}
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve accelerationCurve;
+    [SerializeField] private bool touchControls;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float xLimit;
+
+    private Directions _direction; // false means left, true means right
+    private Rigidbody _rigidbody;
+    private float _accTime;
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!touchControls)
+        {
+            if (!Input.anyKey) _direction = Directions.Straight;
+
+            if (Input.GetKey(KeyCode.A)) _direction = Directions.Left;
+            
+            if (Input.GetKey(KeyCode.D)) _direction = Directions.Right;
+
+        }
+        else
+        {
+            
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        _accTime += Time.deltaTime;
+        _rigidbody.AddForce(Vector3.forward * 10);
+        switch (_direction)
+        {
+            case Directions.Left:
+                GoLeft();
+                break;
+            
+            case Directions.Right:
+                GoRight();
+                break;
+            
+            case Directions.Straight:
+                _accTime = 0;
+                break;
+        }
+        
+        if (_rigidbody.velocity.magnitude > maxVelocity)
+            _rigidbody.velocity = _rigidbody.velocity.normalized * maxVelocity;
+        
+    }
+    
+    private void GoLeft()
+    {
+        _accTime += Time.deltaTime;
+        if (transform.position.x > -xLimit)
+        {
+            _rigidbody.AddForce(Vector3.left * accelerationCurve.Evaluate(_accTime));
+        }
+        else
+        {
+            _rigidbody.velocity = Vector3.forward * maxVelocity;
+        }
+    }
+
+    private void GoRight()
+    {
+        _accTime += Time.deltaTime;
+        if (transform.position.x < xLimit)
+        {
+            _rigidbody.AddForce(Vector3.right * accelerationCurve.Evaluate(_accTime));
+        }
+        else
+        {
+            _rigidbody.velocity = Vector3.forward * maxVelocity;
+        }
+    }
+}
